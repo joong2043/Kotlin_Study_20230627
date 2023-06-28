994313996d90512e53f711626ca58472267f4b57
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-06-28 17:26:19 +0900

    request mapping 주소를 feat1로 변경

--- src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/develop")
+@RequestMapping("/api/feat1")
 @RestController
 class BlogController (
     val blogService: BlogService
