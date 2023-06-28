e39f1af7e02d2c9ef0d324443df58a71960e2b5d
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-06-28 18:09:53 +0900

    request mapping 주소를 changeToFeat3로 변경

--- src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/changeFeat3")
+@RequestMapping("/api/changeToFeat3")
 @RestController
 class BlogController (
     val blogService: BlogService
