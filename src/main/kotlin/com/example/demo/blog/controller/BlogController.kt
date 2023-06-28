7422d82865191b0b60b47193034c458059ce6d01
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-06-28 16:37:58 +0900

    request mapping 주소를 develop로 변경

--- src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/1234")
+@RequestMapping("/api/develop")
 @RestController
 class BlogController (
     val blogService: BlogService
