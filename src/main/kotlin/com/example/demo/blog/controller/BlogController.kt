2d1b1a3994c220a1310ad82816fe460e046c1249
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-06-28 16:02:43 +0900

    request mapping 주소를 1234로 변경

--- src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/cccc")
+@RequestMapping("/api/1234")
 @RestController
 class BlogController (
     val blogService: BlogService
