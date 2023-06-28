68e63dc3c87e6f395ef80f74e30e9ed3297e4290
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-06-28 15:31:28 +0900

    request mapping 주소 변경

--- src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/blog")
+@RequestMapping("/api/bbbb")
 @RestController
 class BlogController (
     val blogService: BlogService
