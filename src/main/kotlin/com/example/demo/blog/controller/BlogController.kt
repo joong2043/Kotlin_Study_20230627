6be7e6a19e9e77fb141aad21f59632e7fe127263
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-07-02 19:22:09 +0900

    Fe

--- src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/demo/blog/controller/BlogController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/3로바꾸기")
+@RequestMapping("/api/blog")
 @RestController
 class BlogController (
     val blogService: BlogService
